06118f762d56c86d27d68f98ee21f96b87492024
Hannah Smith <hls68@byu.edu>
2023-01-25 09:46:49 -0700

    Add project files.

---
+++ GitFun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
